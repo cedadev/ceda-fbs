0b60be2f86bda84af227b974cf7172cfa89f83ed
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-02-29 17:40:39 +0000

    changes for issue #23266.Library used for reading pp files changed from cf to cdms2.

--- python/src/fbs/file_handlers/pp_file.py
+++ python/src/fbs/file_handlers/pp_file.py
@@ -1,4 +1,4 @@
-import cf
+import cdms2 as cdms
 import os
 import fbs_lib.util as util
 
@@ -20,70 +20,6 @@ class PpFile(GenericFile):
     def get_handler_id(self):
         return self.handler_id
 
-    def phenomena(self, phenomena):
-
-        """
-        Construct list of Phenomena based on variables in pp file.
-        :returns : List of phenomena.
-        """
-        phenomenon_attr = {}
-        phenomena_list = []
-        phenomenon_parameters_dict = {}
-        try:
-            number_of_phenomena = len(phenomena)
-            found = set()
-            #For all phenomena.
-            for i in range(0, number_of_phenomena):
-                phen = phenomena[i]
-                list_of_phenomenon_parameters = []
-                list_of_phenomenon_parameters_t = []
-
-                #For every phenomenon.
-                dict_of_phenomenon_prop = phen.properties
-                keys = dict_of_phenomenon_prop.keys()
-                for key in keys:
-                    value = str(dict_of_phenomenon_prop[key])
-
-                    if len(key) < util.NETCDF_MAX_PAR_LENGTH \
-                       and len(value) < util.NETCDF_MAX_PAR_LENGTH:
-                        phenomenon_attr["name"] = key
-                        phenomenon_attr["value"] = value
-
-                        list_of_phenomenon_parameters.append(phenomenon_attr.copy())
-                        #phenomenon_attr.clear()
-
-                        list_of_phenomenon_parameters_t.append((key, value))
-
-                #Also add var_id
-                """
-                phenomenon_attr["name"] = "var_id"
-                if "name" in keys:
-                    phenomenon_attr["value"] = str(dict_of_phenomenon_prop["name"])
-                else:
-                    phenomenon_attr["value"] = "None"
-
-                #Attributes of phenomenon.
-                list_of_phenomenon_parameters.append(phenomenon_attr.copy())
-                phenomenon_attr.clear()
-                """
-
-                #Dict of phenomenon attributes.
-                phenomenon_parameters_dict["phenomenon_parameters"] = list_of_phenomenon_parameters
-                list_of_phenomenon_parameters_tt = tuple(list_of_phenomenon_parameters_t)
-
-
-                if list_of_phenomenon_parameters_tt not in found:
-                    found.add(list_of_phenomenon_parameters_tt)
-                    #list of phenomenon.
-                    phenomena_list.append(phenomenon_parameters_dict.copy())
-                    phenomenon_parameters_dict.clear()
-                else:
-                    phenomenon_parameters_dict.clear()
-
-            return phenomena_list
-        except Exception as ex:
-            return None
-
     def get_properties_pp_level2(self):
         """
         Wrapper for method phenomena().
@@ -94,93 +30,49 @@ class PpFile(GenericFile):
         file_info = self.get_properties_generic_level1()
 
         if file_info is not None:
-
-            self.handler_id = "pp handler level 2."
-            #level 2
-            #kltsa 10/02/2016: change for issue 23266, 
-            #every file will have its own directory for stroring temp data.
-            file_temp_dir = os.path.join(self.additional_param, os.path.basename(self.file_path))
-            if not os.path.exists(file_temp_dir):
-                os.makedirs(file_temp_dir)
-
-            phenomena = None
-            pp_phenomena = None
             try:
-                cf.TEMPDIR(file_temp_dir) #create derectory for each file
-                phenomena = cf.read(self.file_path)
-                pp_phenomena = self.phenomena(phenomena)
-                phenomena = None
-            except Exception: #if read fails then delete temporary folder.
-                util.delete_folder(file_temp_dir)
-            else:
-                util.delete_folder(file_temp_dir)
+                self.handler_id = "pp handler level 2."
+                phenomena_list = []
+                phenomenon_parameters_dict = {}
+                list_of_phenomenon_parameters = []
+                phenomenon_attr = {}
+                all_attr = {}
 
-            if pp_phenomena is None:
+                pp_file_content=cdms.open(self.file_path)
+                var_ids = pp_file_content.listvariables()
+
+                #Filter long values and overwrite duplicates.
+                for var_id in var_ids:
+                    metadata_dict = pp_file_content[var_id].attributes
+                    for key in metadata_dict.keys():
+                        value = str(metadata_dict[key])
+
+                        if     len(key) < util.MAX_PAR_LENGTH \
+                           and len(value) < util.MAX_PAR_LENGTH:
+                            all_attr[key] = value
+
+                #Store each key and value.
+                for key in all_attr.keys():
+                    value = str(all_attr[key])
+                    phenomenon_attr["name"] = key
+                    phenomenon_attr["value"] = value
+                    list_of_phenomenon_parameters.append(phenomenon_attr.copy())
+
+
+                #Dict of phenomenon attributes.
+                phenomenon_parameters_dict["phenomenon_parameters"] = list_of_phenomenon_parameters
+                phenomena_list.append(phenomenon_parameters_dict.copy())
+
+                pp_file_content.close()
+                file_info["phenomena"] = phenomena_list
                 return file_info
-
-            file_info["phenomena"] = pp_phenomena
-
-            return file_info
-
+            except Exception as ex:
+                return None
         else:
             return None
-
-    def normalize_coord(self, coord):
-        if coord > 180:
-            coord = coord - 360
-        return coord
 
     def get_properties_pp_level3(self):
-        #Get basic file info.
-        file_info = self.get_properties_generic_level1()
-
-        if file_info is not None:
-
-            self.handler_id = "pp handler level 2."
-            #level 2
-            cf.TEMPDIR(self.additional_param)
-            phenomena = cf.read(self.file_path)
-            pp_phenomena = self.phenomena(phenomena)
-
-            if pp_phenomena is None:
-                return file_info
-
-            file_info["phenomena"] = pp_phenomena
-
-            #geospatial data.
-            list_max_lat = []
-            list_min_lat = []
-            list_max_lon = []
-            list_min_lon = []
-            for item in phenomena:
-                dim_dict_lat = item.coords('lat')
-                dim_dict_lon = item.coords('lon')
-                for key in dim_dict_lat:
-                    list_max_lat.append(float(str(item.coords('lat')[key].max()).split()[0]))
-                    list_min_lat.append(float(str(item.coords('lat')[key].min()).split()[0]))
-
-                for key in dim_dict_lon:
-                    list_max_lon.append(float(str(item.coords('lon')[key].max()).split()[0]))
-                    list_min_lon.append(float(str(item.coords('lon')[key].min()).split()[0]))
-
-            max_lat = self.normalize_coord(max(list_max_lat))
-            min_lat = self.normalize_coord(min(list_min_lat))
-            max_lon = self.normalize_coord(max(list_max_lon))
-            min_lon = self.normalize_coord(min(list_min_lon))
-
-            file_info["spatial"] = {'coordinates': {'type': 'envelope', 'coordinates': [[max_lat, max_lon], [max_lon, min_lon]]}}
-
-
-            return file_info
-
-        else:
-            return None
-
-
-        res = self.get_properties_pp_level2()
-        #ok lets add geospatial information.
-        self.handler_id = "pp handler level 3."
-        return res
+        return None
 
     def get_properties(self):
 
