add3f65650f2331251e1fdb8c2d371a4a268b13d
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-04-07 15:59:11 +0100

    Changes for issue 23281.

--- python/src/test/test_relations/content.py
+++ python/src/test/test_relations/content.py
@@ -1,5 +1,7 @@
 import random, string
-#mappings...
+from copy import deepcopy
+
+#mappings of index.
 mapping = {
  "mappings" : 
  {
@@ -88,18 +90,16 @@ test_file_dict = {
 }
 
 def get_file_doc(name):
+
+    """
+    Returns a valid jason document for the file type.
+    """
+
     d = test_file_dict.copy()
     d["info"]["name"] = name
     d["info"]["name_auto"] = name
     return d
 
-OLD_test_phenomenon = {"parameters":
-    [
-    {"name": "phenomenon attr1", "value": "phenomenon value1"},
-    {"name": "phenomenon attr2", "value": "phenomenon value2"},
-    {"name": "phenomenon attr3", "value": "phenomenon value3"},
-    {"name": "phenomenon attr4", "value": "phenomenon value4"}
-  ]}
 
 #phenomena contained in the files.
 #rainfall: {"long_name": "rainfall", "units": "mm"}
@@ -151,12 +151,18 @@ pressure_sub =\
   ]
 }
 
+#Phenomena that will be contained in every file.
 phen = [rainfall, temperature, pressure, pressure_sub]
 
 def randomword(length):
     return ''.join(random.choice(string.lowercase) for i in range(length))
 
 def create_random_phenomenon():
+
+    """
+    Returns a phenomenon with random name and value.
+    """
+
     name = randomword(10)
     value = randomword(10)
 
@@ -169,52 +175,46 @@ def create_random_phenomenon():
     return phen.copy()
 
 def get_file_phenomena():
+
+    """
+    Returns phenomena contained in a file.
+    The function returns one unique phenomeon for every file.
+    """
+
     random_phen = create_random_phenomenon()
     #print "Random phenomenon created :"
     #print random_phen
     file_phenomena = phen[:]
-    file_phenomena.append(random_phen)
+    file_phenomena_real = deepcopy(file_phenomena)
+    file_phenomena_real.append(random_phen)
     #print file_phenomena
-    return file_phenomena
+    return file_phenomena_real
+
+def get_file_phenomena_i(index):
+
+    """
+    Return file phenomena depending on the value of index.
+    """
+
+    if index <= 99:
+        return [phen[0]]
+    elif index >= 100 and index <= 199:
+        return [phen[1]]
+    elif index >= 200 and index <= 299:
+        return [phen[2]]
+    elif index >= 300 and index <= 399:
+        return [phen[3]]
+    elif index >= 400 and index <= 499:
+        return phen[0:2]
+    elif index >= 500 and index <= 599:
+        return phen[0:3]
+    elif index >= 600 and index <= 699:
+        tmp_l = phen[0:2]
+        tmp_l.append(phen[3])
+        return tmp_l
 
 def get_phenomenon(s):
     return phen[s]
-
-"""
-curl -XPOST "mist.badc.rl.ac.uk:9200/archive_level2/phenomenon/?pretty=true" -d' {
-  "id": "2017",
-
-
-}
-"""
-
-
-# query that can be used for searching for specific phenomenon:
-query_phen = {
- "query":
- {
-  "nested":
-  {
-   "path": "parameters",
-   "query":
-   {
-    "bool":
-    {
-     "must":
-     [
-      { "match_phrase": { "parameters.name": "phenomenon attr100" } },
-      { "match_phrase": { "parameters.value": "phenomenon value100" } }
-     ]
-    }
-   }
-  }
- },
-    "size": 10
-}
-
-# query for searching for documents with specific phenomenon id:
-
-# http://mist.badc.rl.ac.uk:9200/archive_level2/file/_search?q=info.phenomena:2016&pretty=true;
 
 
 
