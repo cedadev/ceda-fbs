609f67468b86c177bda0a3f0f09cb22a2402775c
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-04-12 10:45:51 +0100

    Changes for issue 23281.

--- python/src/fbs/file_handlers/netcdf_file.py
+++ python/src/fbs/file_handlers/netcdf_file.py
@@ -4,7 +4,6 @@ import netCDF4
 from fbs.file_handlers.generic_file import GenericFile
 import fbs_lib.util as util
 import fbs_lib.geojson as geojson
-from es import ops
 
 class   NetCdfFile(GenericFile):
     """
