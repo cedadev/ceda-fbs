84008cfa30ab7fc95e7c46bed52d3229d356d770
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-05-13 14:07:30 +0100

    changes for issue 23290.

--- python/src/fbs/file_handlers/manifest_file.py
+++ python/src/fbs/file_handlers/manifest_file.py
@@ -6,72 +6,182 @@ Created on 12 May 2016
 
 from fbs.file_handlers.generic_file import GenericFile
 import fbs_lib.util as util
-import xmltodict
+
+import datetime, os
+import xml.etree.cElementTree as ET
+
+# Set up name spaces for use in XML paths
+ns = {
+    "gml": "http://www.opengis.net/gml",
+    "gx": "http://www.google.com/kml/ext/2.2",
+    "s1": "http://www.esa.int/safe/sentinel-1.0/sentinel-1",    
+    "s1sar": "http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar",
+    "s1sarl1": "http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar/level-1",
+    "s1sarl2": "http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar/level-2",
+    "safe": "http://www.esa.int/safe/sentinel-1.0",    
+    "version": "esa/safe/sentinel-1.0/sentinel-1/sar/level-1/slc/standard/iwsp",
+    "xfdu": "urn:ccsds:schema:xfdu:1",
+}
+
+# Define mappings dictionary of XML paths to sections we are capturing
+mappings = {
+    "platform": {
+       "common_prefix": 
+         "./metadataSection/metadataObject[@ID='platform']/metadataWrap/xmlData/",
+       "properties": {
+         "Platform Family Name": "{%(safe)s}platform/{%(safe)s}familyName" % ns, 
+         "NSSDC Identifier": "{%(safe)s}platform/{%(safe)s}nssdcIdentifier" % ns,
+         "Instrument Family Name": "{%(safe)s}platform/{%(safe)s}instrument/{%(safe)s}familyName" % ns,
+         "Mode": "{%(safe)s}platform/{%(safe)s}instrument/{%(safe)s}extension/{%(s1sarl1)s}instrumentMode/{%(s1sarl1)s}mode" % ns,
+       },
+    },
+    "spatial": {
+       "common_prefix":
+         "./metadataSection/metadataObject[@ID='measurementFrameSet']/metadataWrap/xmlData/",
+       "transformers": {
+         # Defines method name used to convert string of coordinates into list of tuples containing floats
+         "Coordinates": "_package_coordinates",
+       },
+       "properties": {
+         "Coordinates": "{%(safe)s}frameSet/{%(safe)s}frame/{%(safe)s}footPrint/{%(gml)s}coordinates" % ns,
+       },
+    },
+    "product_info": {
+       "common_prefix":
+         "./metadataSection/metadataObject[@ID='generalProductInformation']/metadataWrap/xmlData/",
+       "properties": {
+         "Product Class": "{%(s1sarl1)s}standAloneProductInformation/{%(s1sarl1)s}productClass" % ns,
+         "Product Class Description": "{%(s1sarl1)s}standAloneProductInformation/{%(s1sarl1)s}productClassDescription" % ns,
+         "Timeliness Category": "{%(s1sarl1)s}standAloneProductInformation/{%(s1sarl1)s}productTimelinessCategory" % ns,
+         "Product Composition": "{%(s1sarl1)s}standAloneProductInformation/{%(s1sarl1)s}productComposition" % ns,
+         "Polarisation": "{%(s1sarl1)s}standAloneProductInformation/{%(s1sarl1)s}transmitterReceiverPolarisation" % ns,
+       },
+    },
+    "orbit_info": {
+       "common_prefix":
+         "./metadataSection/metadataObject[@ID='measurementOrbitReference']/metadataWrap/xmlData/",
+       "properties": {
+         "Start Relative Orbit Number": "{%(safe)s}orbitReference/{%(safe)s}relativeOrbitNumber[@type='start']" % ns,
+         "Stop Relative Orbit Number": "{%(safe)s}orbitReference/{%(safe)s}relativeOrbitNumber[@type='stop']" % ns,
+         "Start Orbit Number": "{%(safe)s}orbitReference/{%(safe)s}orbitNumber[@type='start']" % ns,
+         "Stop Orbit Number": "{%(safe)s}orbitReference/{%(safe)s}orbitNumber[@type='stop']" % ns,
+         "Pass Direction": "{%(safe)s}orbitReference/{%(safe)s}extension/{%(s1)s}orbitProperties/{%(s1)s}pass" % ns,
+         "Phase Identifier": "{%(safe)s}orbitReference/{%(safe)s}phaseIdentifier" % ns,
+         "Cycle Number": "{%(safe)s}orbitReference/{%(safe)s}cycleNumber" % ns,
+       },
+    },
+    "acquisition_period": {
+       "common_prefix":
+         "./metadataSection/metadataObject[@ID='acquisitionPeriod']/metadataWrap/xmlData/",
+       "properties": {
+         "Start Time": "{%(safe)s}acquisitionPeriod/{%(safe)s}startTime" % ns,
+         "Stop Time": "{%(safe)s}acquisitionPeriod/{%(safe)s}stopTime" % ns,
+       }
+    }
+}
+
 
 class ManifestFile(GenericFile):
+
     """
     Class for returning basic information about the content
-    of an nasaames file.
+    of an manifest file.
     """
 
     def __init__(self, file_path, level, additional_param=None):
         GenericFile.__init__(self, file_path, level)
-        self.handler_id = "MAnifest handler level 3."
-        self.FILE_FORMAT = "NASA Ames"
+        self.handler_id = "Manifest handler level 3."
+        self.FILE_FORMAT = "Manifest"
 
     def get_handler_id(self):
         return self.handler_id
 
-    def find_elements(self, node, item_list, result):
-        for key, item in node.items():
-            if isinstance(item, dict):
-                self.find_elements(item, item_list, result)
-            else:
-                if isinstance(item, list):
-                    for sub_item in item:
-                        if isinstance(sub_item, dict):
-                            self.find_elements(sub_item, item_list, result)
-                        else:
-                            #print key, " : ", item
-                            if key in item_list:
-                                result.append(item)
-                else:
-                    #print key, " : ", item
-                    if key in item_list:
-                        result.append(key)
-                        result.append(item)
+    def open_file(self):
+        self.root = ET.parse(self.file_path).getroot()
+        self._parse_content()
+        return self
+
+    def _parse_content(self):
+        self.sections = {}
+
+        for section_id, content_dict in mappings.items():
+            self.sections[section_id] = {}
+            prefix = content_dict["common_prefix"]
+
+            for item_name, xml_path_end in content_dict["properties"].items():
+                xml_path = prefix + xml_path_end
+
+                try:
+                    value = self.root.find(xml_path).text
+                    if item_name in content_dict.get("transformers", {}):
+                        transformer = getattr(self, content_dict["transformers"][item_name])
+                        value = transformer(value)
+
+                    self.sections[section_id][item_name] = value
+                except:
+                    print "FAILED: %s  -->  %s" % (section_id, xml_path)
+
+
+    def _package_coordinates(self, coords_string):
+        """
+        Converts a coordinates string into a dictionary of lats and lons.
+        :param string coords_string: a string of lat,lon pairs separated by commas
+        :returns: Dictionary of: {"lats": <list of lats>, "lons": <list of lons>}
+        """
+        coord_pairs = coords_string.split()
+        lats = []
+        lons = []
+
+        for ll_string in coord_pairs:
+            lat, lon = ll_string.split(",")
+            lats.append(float(lat))
+            lons.append(float(lon))
+
+        return {"lat": lats, "lon": lons, "type": "swath"}
+
+    def get_geospatial(self):
+        """
+        Return coordinates.
+        :returns: Dict containing geospatial information.
+        """
+        return self.sections["spatial"]["Coordinates"]
+
+    def get_temporal(self):
+        """
+        Returns temporal window.
+
+        :returns: List containing temporal metadata
+        """
+        ap = self.sections["acquisition_period"]
+        return {"start_time": ap["Start Time"],
+                "end_time": ap["Stop Time"]}
+
 
     def get_metadata_manifest_level3(self):
         self.handler_id = "MAnifest handler level 3."
 
         res = self.get_metadata_generic_level1()
 
-        elements_to_find = [
-                            "safe:startTime",
-                            "safe:stopTime",
-                            "gml:coordinates"
-                           ]
+        self.open_file()
+        geospatial = self.get_geospatial()
+        temporal = self.get_temporal()
 
-        document_file = open(self.file_path, "r") # Open a file in read-only mode
-        original_doc = document_file.read()       # read the file object
-        document = xmltodict.parse(original_doc)  # Parse the read document string
-        print document
+        #print geospatial
+        #print temporal
 
-        result = []
-        self.find_elements(document, elements_to_find, result)
+        #{'lat': [-56.301735, -54.830536, -58.449139, -60.058411], 'type': 'swath', 'lon': [-37.49408, -31.223965, -28.045124, -34.86174]}
+        #{'start_time': '2015-07-04T21:33:30.724498', 'end_time': '2015-07-04T21:34:36.080966'}
 
-        #print "================"
-        #for item in result:
-        #    print item  
-        #u'-56.301735,-37.494080 -54.830536,-31.223965 -58.449139,-28.045124 -60.058411,-34.861740
 
-        c1 = (result[5]).split(" ")[0].split(",")[0]
-        c2 = (result[5]).split(" ")[1].split(",")[1]
-        c3 = (result[5]).split(" ")[3].split(",")[0]
-        c4 = (result[5]).split(" ")[3].split(",")[1]
+        l1  =  geospatial["lat"][0]
+        l2 =  geospatial["lat"][1]
 
-        res[0]["info"]["spatial"] =  {'coordinates': {'type': 'envelope', 'coordinates': [[c1, c2], [c3, c4]] } }
-        res[0]["info"]["temporal"] = {'start_time': result[1], 'end_time': result[3] }
+        lo1  = geospatial["lon"][0]
+        lo2 = geospatial["lon"][1]
+
+
+        res[0]["info"]["spatial"] =  {'coordinates': {'type': 'envelope', 'coordinates': [[l1, lo1], [l2, lo2]] } }
+        res[0]["info"]["temporal"] = {'start_time': temporal["start_time"], 'end_time': temporal["end_time"] }
 
         return res
 
