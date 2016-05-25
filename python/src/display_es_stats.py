1f1e283a1fc8aff2198ff30714d3d74f30eccc7f
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-05-25 10:42:56 +0100

    Minor change in naming of file.

---
+++ python/src/display_es_stats.py
@@ -0,0 +1,65 @@
+'''
+Created on 24 May 2016
+
+@author: kleanthis
+'''
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import time
+from elasticsearch import Elasticsearch
+import datetime
+import fbs_lib.util as util
+import os
+
+def read_cfg():
+
+    c_dir     = os.path.dirname(__file__)
+    conf_path = os.path.join(c_dir, "../config/ceda_fbs.ini")
+    config    = util.cfg_read(conf_path)
+
+    return config
+
+def plot(x, y):
+
+    # The X axis can just be numbered 0,1,2,3...
+    x_axis = np.arange(len(x))
+
+    plt.bar(x_axis, y)
+
+    plt.xticks(x_axis + 0.5, x, rotation=25)
+    plt.show()
+
+
+def display_stats():
+
+    cfg = read_cfg()
+
+    filename =  os.path.join(cfg["core"]["log-path"], "fbs-stats.txt")
+
+    file_contents = util.read_file_into_list(filename)
+
+    x_axis = []
+    y_axis = []
+    for item in file_contents:
+        values = item.split(",")
+        x_axis.append(values[1])
+        y_axis.append(int(values[3].rstrip()))
+
+    plot(x_axis, y_axis)
+
+
+def main():
+
+    start = datetime.datetime.now()
+    print "Script started at: %s" %(str(start))
+
+    display_stats()
+
+    end = datetime.datetime.now()
+    print "Script ended at : %s it ran for : %s" %(str(end), str(end - start))
+
+if __name__ == "__main__":
+    main()
