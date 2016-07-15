553cd1e295ba4cc3d7582d9b99ba258de4ee8d9b
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2016-07-15 16:12:17 +0100

    Values in coordinates rounded.

--- python/src/fbs/file_handlers/esasafe_file.py
+++ python/src/fbs/file_handlers/esasafe_file.py
@@ -175,7 +175,7 @@ class EsaSafeFile(GenericFile):
         lon_l =  min(geospatial["lon"])
 
 
-        spatial = {"coordinates": {"type": "envelope", "coordinates": [[lon_l, lat_l], [lon_u, lat_u]] } }
+        spatial = {"coordinates": {"type": "envelope", "coordinates": [[round(lon_l), round(lat_l)], [round(lon_u), round(lat_u)]] } }
         res[0]["info"]["temporal"] = {"start_time": temporal["start_time"], "end_time": temporal["end_time"] }
 
         phenomena = None
