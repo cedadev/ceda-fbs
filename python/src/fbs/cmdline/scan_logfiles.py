7d083e5bc4009b3b743df98d573ed0e67d5019bb
agstephens <ag.stephens@stfc.ac.uk>
2017-02-09 15:59:35 +0000

    Improved logfile scanner.

--- python/src/fbs/cmdline/scan_logfiles.py
+++ python/src/fbs/cmdline/scan_logfiles.py
@@ -5,7 +5,6 @@ Usage:
   scan_dataset.py -h | --help
   scan_dataset.py --version
   scan_dataset.py (-l <log_directory> | --log_directory <log_directory>)
-   (-f <filename> | --filename <filename>)
 
 Options:
   -h --help                           Show this screen.
@@ -21,59 +20,48 @@ from cmdline import __version__
 
 import datetime
 
+
 def parse_logs(com_args):
-
     log_directory = com_args["log_directory"]
-    datasets_file = com_args["filename"]
+#    datasets_file = com_args["filename"]
 
-    #find all files in log directroy.
-    list_of_files = util.build_file_list(log_directory)
+    # find all files in log directroy.
+    list_of_files = sorted(util.build_file_list(log_directory))
     num_files = len(list_of_files)
-
     summary_info = {}
 
-    #open each file and exrtact info.
+    # open each file and exrtact info.
     for i in range(0, num_files):
 
         filename = list_of_files[i]
         content_list = util.read_file_into_list(filename)
-        summary = util.find_in_list(content_list, "Summary")
 
-        if summary is not None:
-            words_list = summary.split("Summary", 1)[1].split(",")
-            #dataset
-            dataset = (words_list[0].split())[5]
-            #indexed
+        summary = [item for item in content_list if item.find("Summary") > -1] 
+
+        if summary:
+            words_list = summary[0].split("Summary", 1)[1].split(",")
+            if "".join(words_list).find("files indexed") < 0: continue
+
+            dataset = words_list[0].split()[5]
             indexed = int(words_list[1].split()[3])
-            #database errors
+
             database_errors = int(words_list[2].split()[3])
-            #properties errors
             properties_errors = int(words_list[3].split()[3])
-            #total files
             total_files = int(words_list[4].split()[3])
 
-            if dataset not in  summary_info:
-                dataset_info = {}
-                #dataset_info["dataset"] = dataset
-                dataset_info["indexed"] = indexed
-                dataset_info["database_errors"] = database_errors
-                dataset_info["properties_errors"] = properties_errors
-                dataset_info["total_files"] = total_files
-                dataset_info["dataset_dir"] = util.find_dataset(datasets_file, dataset)
-
-                summary_info[dataset] = dataset_info.copy()
-                dataset_info = None
+            if dataset in summary_info:
+                info = summary_info[dataset] 
+                info["indexed"] += indexed
+                info["database_errors"] += database_errors
+                info["properties_errors"] += properties_errors
             else:
-                dataset_info = {}
-                dataset_info = summary_info[dataset]
-                dataset_info["indexed"] = dataset_info["indexed"] + indexed
-                dataset_info["database_errors"] = dataset_info["database_errors"] + database_errors
-                dataset_info["properties_errors"] = dataset_info["properties_errors"] + properties_errors
-                #dataset_info["total_files"] = dataset_info["total_files"] + total_files
-                dataset_info = None
-        #At the end print all information.
+                summary_info[dataset] = {"indexed": indexed,
+                                         "database_errors": database_errors,
+                                         "properties_errors": properties_errors,
+                                         "total_files": total_files}
 
     return summary_info
+
 
 def validate_results(dict_data):
 
@@ -102,18 +90,20 @@ def validate_results(dict_data):
 
     dict_data["Totals"] = datasets_info.copy()
 
+
 def print_dict(dict_data):
-    print "{a:<40} {b:<20} {c:<20} {d:<20} {e:<20} {g:<10} {f:<20}".format(a="Dataset",\
-           b="Indexed", c="Total files", d="Properties errors", e="Database errors",\
-           f="Dataset dir", g="Status")
+
+    print "{a:<40} {b:<20} {c:<20} {d:<20} {e:<20} {g:<10}".format(a="Dataset",
+           b="Indexed", c="Total files", d="Properties errors", e="Database errors",
+           g="Status")
 
     print ("-------------------------------------------------------------------------"
            "--------------------------------------------------------------------------")
 
-    for item in dict_data:
+    for item in sorted(dict_data.keys()):
         if item != "Totals":
             print ("{a:<40} {p[indexed]:<20} {p[total_files]:<20} {p[properties_errors]:<20}" 
-                " {p[database_errors]:<20} {p[status]:<10} {p[dataset_dir]:<20}").format(a=item, p=dict_data[item])
+                " {p[database_errors]:<20} {p[status]:<10}").format(a=item, p=dict_data[item])
 
     print ("-------------------------------------------------------------------------"
            "--------------------------------------------------------------------------")
@@ -124,10 +114,6 @@ def print_dict(dict_data):
 
 
 def main():
-
-    """
-    Relevant ticket : http://team.ceda.ac.uk/trac/ceda/ticket/23235
-    """
     #Get command line arguments.
     com_args = util.sanitise_args(docopt(__doc__, version=__version__))
     start = datetime.datetime.now()
@@ -139,6 +125,7 @@ def main():
 
     end = datetime.datetime.now()
     print "\nScript ended at : {} it ran for : {}".format(str(end), str(end - start))
+
 
 if __name__ == '__main__':
     main()
