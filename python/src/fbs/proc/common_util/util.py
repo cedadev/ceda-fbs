138898030c1df2a62dab84bba8d06b3953f93c98
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-04-29 15:19:35 +0100

    Fixed issue with string and bytes

--- python/src/fbs/proc/common_util/util.py
+++ python/src/fbs/proc/common_util/util.py
@@ -373,7 +373,7 @@ def get_number_of_submitted_lotus_tasks(max_number_of_tasks_to_submit):
     empty_task_queue_string = "No unfinished job found\n"
     non_empty_task_queue_string = "JOBID     USER    STAT  QUEUE      FROM_HOST   EXEC_HOST   JOB_NAME   SUBMIT_TIME"
 
-    command_output = subprocess.check_output('bjobs', stderr=subprocess.STDOUT, shell=True)
+    command_output = subprocess.check_output('bjobs', stderr=subprocess.STDOUT, shell=True).decode('utf-8', errors='ignore')
 
     if command_output == empty_task_queue_string:
         num_of_running_tasks = 0
